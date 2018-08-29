d2680d3395a87fc4027b6d945b4c4c813ebc8c76
Reginorkk <lucascifuentes1@hotmail.com>
2018-08-29 19:03:38 -0300

    a

--- inc/navbarblank.php
+++ inc/navbarblank.php
@@ -1,3 +1,3 @@
 <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="inicio"> <img src="img/logo.png" width="269" height="71" alt="CozyCopy"></a>
+      <a class="navbar-brand" href="inicio"> <img src="../img/logo.png" width="269" height="71" alt="CozyCopy"></a>
     </nav>
