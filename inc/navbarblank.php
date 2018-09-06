cc166a57084332252645c874e79aeb9fbb174a74
Reginorkk <lucascifuentes1@hotmail.com>
2018-09-06 12:11:49 -0300

    arreglo del index + pocos retoques esteticos

--- inc/navbarblank.php
+++ inc/navbarblank.php
@@ -1,3 +1,3 @@
 <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand"> <img src="../img/logo.png" width="269" height="71" alt="CozyCopy"></a>
+      <a class="navbar-brand"> <img src="img/logo.png" width="269" height="71" alt="CozyCopy"></a>
     </nav>
--- index.php
+++ index.php
@@ -3,10 +3,24 @@
     <meta charset="utf-8" />
     <title>Cozy Copy</title>
     <meta name="viewport" content="width=device-width, user-scalabe=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <?php include('inc/head.php'); ?>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/estilo.css">
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </head>
 <body>
 <?php include('inc/navbarblank.php'); ?>
+
+<p class="textoprincipal">Registrate, inscribite un tu curso y descargá tus apuntes</p>
+
+<footer class="page-footer font-small fixed-bottom ml-2 mb-2">
+    
+<a href="">¿Quienes somos?</a> -
+<a href="">¿Cuales son nuestros objetivos?</a> -
+<a href="">Contacto</a>
+
+</footer>
 
 </body>
 </html>
